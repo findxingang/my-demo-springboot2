3d331bfd1b91f46e1bd0f3c84230fe9b4e1b0876
xingang <findxingang@163.com>
2024-03-08 09:11:55 +0800

    demo-04-java
    Java并发-volatile不保证原子性

---
+++ demo-04-java/src/main/java/pers/xingang/demo/concurrent/volatile_demo/VolatileAtomicityDemo.java
@@ -0,0 +1,32 @@
+package pers.xingang.demo.concurrent.volatile_demo;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * @author xingang
+ * @since 2024/03/06 16:41
+ */
+public class VolatileAtomicityDemo {
+    public volatile static int inc = 0;
+
+    public void increase() {
+        inc++;
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        ExecutorService threadPool = Executors.newFixedThreadPool(5);
+        VolatileAtomicityDemo volatileAtomicityDemo = new VolatileAtomicityDemo();
+        for (int i = 0; i < 5; i++) {
+            threadPool.execute(() -> {
+                for (int j = 0; j < 500; j++) {
+                    volatileAtomicityDemo.increase();
+                }
+            });
+        }
+        // 等待1.5秒，保证上面程序执行完成
+        Thread.sleep(1500);
+        System.out.println(inc);
+        threadPool.shutdown();
+    }
+}
